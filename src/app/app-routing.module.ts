a3c77b7ce01c536cea801857c38bf665a50f3502
Martin Yankov <martinyankov@Martins-MacBook-Pro.local>
2022-01-26 23:43:04 +0100

    added three and items list

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { ItemDetailComponent } from './item-detail/item-detail.component';
+import { ItemsComponent } from './items/items.component';
+
+const routes: Routes = [
+  { path: '', redirectTo: '/items', pathMatch: 'full' },
+  { path: 'detail/:id', component: ItemDetailComponent },
+  { path: 'items', component: ItemsComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ItemsComponent } from './items/items.component';
 
 @NgModule({
   declarations: [
@@ -10,7 +11,9 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ItemsComponent
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
