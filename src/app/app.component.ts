a6338276f5086fe9097675a8dc720a1540a5a794
Martin Yankov <martinyankov@Martins-MacBook-Pro.local>
2022-01-27 01:00:21 +0100

    implement getDataFromAPI to connect with nodejs

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { AppServiceService } from './app-service.service';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,20 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = '3D configurator';
+
+  constructor(private service: AppServiceService){}
+
+  ngOnInit(): void {
+    this.getDataFromAPI();
+  }
+
+  getDataFromAPI(){
+    this.service.getData().subscribe((response) =>{
+      console.log('Response from API is ' + response)
+    }, (error)=>{
+      console.log('Error is ', console.error);
+
+    }
+    )
+  }
 }
